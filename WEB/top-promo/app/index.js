1e6cb2a0fcf3227f3f38b66daf55b08c7a533be7
bash62 <bash62@protonmail.com>
2023-06-08 23:53:30 +0200

    add TOP promo

--- WEB/top-promo/app/index.js
+++ WEB/top-promo/app/index.js
@@ -1,7 +1,9 @@
+const { log } = require("console");
 const express = require("express");
 const app = express();
 const path = require("path");
-
+//load env
+require('dotenv').config();
 
 app.use(express.urlencoded({ extended: true })); // Middleware pour traiter les données POST
 
@@ -11,15 +13,18 @@ app.get('/', function(req, res) {
 
 app.post('/ajouter', function(req, res) {
     // Récupérer les données du formulaire
+    console.log(req.body)
     const id = req.body.id;
     const nom = req.body.nom;
-    const classement = req.body.classement;
+    const note = req.body.note;
     const signature = req.body.signature;
 
-    if(signature == calculerSignature(classement)) {
+    console.log("id : " + calculerSignature(note) + " " + signature);
+    if(signature == calculerSignature(note)) {
         return res.status(200).send({"data":process.env.FLAG});
     } else {
-        return res.status(400).send({"data":'Vilain tricheur ! '});
+        return res.status(400).send({"data":'Vilain tricheur ! ',"message":"La signature est incorrecte !"});
+
     }
 
 });
